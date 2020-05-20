61fb84255c90cbcab5ecc8a48a665afdc5b351a5
Temitope Akinsoto <takinsoto@gmail.com>
2020-05-21 00:30:29 +0100

    add mongo URI keys

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://temitope:THANKgod123$@cluster0-lg3pk.mongodb.net/test?retryWrites=true&w=majority'
+}   
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+const port = process.env.PORT || 5000
 const app = express()
 
-const port = process.env.PORT || 5000
+// DB config
+const db = require('./config/keys').mongoURI
+
+mongoose.connect(db)
+.then(() => {
+    console.log("mongoose connected")
+})
+.catch(e => {
+    console.log(e)
+})
 
 app.get('/', (req, res) => {
     res.send("Welcome to our home route")
