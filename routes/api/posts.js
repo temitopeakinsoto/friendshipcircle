5ec565d7ebf8daf241a062c5ad44328eee43e5d7
Temitope Akinsoto <takinsoto@gmail.com>
2020-08-14 23:37:59 +0100

    add routes description

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// @route  GET api/users/test
+// @desc   Tests posts route
+// @access Public
+
 router.get('/test', (req, res) => {
     res.json({
         msg: "Post endpoint works",
