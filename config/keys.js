e13d6df57452b73004519e647b9cdc422fe10987
Temitope Akinsoto <takinsoto@gmail.com>
2020-08-16 12:29:32 +0100

    add secret key to config

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
     mongoURI: 'mongodb+srv://temitope:THANKgod123$@cluster0.pfbe1.mongodb.net/frienshipcircle?retryWrites=true&w=majority',
+    SECRET: 'You shall not pass!'
 }   
